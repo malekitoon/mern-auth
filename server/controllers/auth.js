39557f38f9299b724ea475e95d30800ff9373228
malekitoon <malekitoon@yahoo.com>
2020-10-13 22:45:02 +0300

    19. Signup user

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,26 @@
+const User = require('../models/User');
+
 exports.signup = (req, res) => {
-  console.log('REQ BODY ON SIGN UP', req.body);
-  
-  res.json({
-    data: 'You hit sign up endpoint!',
-  });
+  const { name, email, password } = req.body;
+
+  User
+    .findOne({ email })
+    .exec((err, user) => {
+      if (user) {
+        return res.status(400).json({ error: 'Email is taken'});
+      }
+
+      let newUser = new User({ name, email, password });
+
+      newUser.save((err, success) => {
+        if (err) {
+          console.log('SIGNUP ERROR', err);
+          return res.status(400).json({ error: err });
+        }
+
+        return res.json({
+          message: 'Sing up success! Please sign in'
+        })
+      });
+    });
 };
--- server/models/User.js
+++ server/models/User.js
@@ -35,8 +35,8 @@ const userSchema = mongoose.Schema({
 userSchema.virtual('password')
   .set(function(password) {
     this._password = password;
-    this.solt = this.makeSalt();
-    this.hashed_password = this.encryptPassword(password)
+    this.salt = this.makeSalt();
+    this.hashed_password = this.encryptPassword(password);
   })
   .get(function() {
     return this._password;
@@ -61,7 +61,7 @@ userSchema.methods = {
   },
 
   makeSalt: function() {
-    return Math.round(new Date().valueOf * Math.random()) + '';
+    return Math.round(new Date().valueOf() * Math.random()) + '';
   }
 };
 
