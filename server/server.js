adaffa007336bc2c96be5799c2fad91e3c188fc2
malekitoon <malekitoon@yahoo.com>
2020-10-13 20:04:22 +0300

    13. Applying middlewares

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
 const express = require('express');
-const PORT = process.env.port || 8000;
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const port = process.env.PORT || 8000;
 
 const app = express();
 
 // import routes
 const authRoutes = require('./routes/auth');
 
+// app middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+// app.use(cors()); // allows all origins
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({ origin: `http://localhost:3000` }));
+}
+
 // middlewares
 app.use('/api', authRoutes);
 
-app.listen(PORT, () => {
-  console.log(`API is now running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`API is now running on port ${port}`);
 });
