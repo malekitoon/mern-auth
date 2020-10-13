3edf82b6cceebdc2c9141efd6e235d14d4a4dcf1
malekitoon <malekitoon@yahoo.com>
2020-10-13 21:39:30 +0300

    16. Connect to mongodb

--- server/server.js
+++ server/server.js
@@ -9,6 +9,23 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('MongoDB Connected...');
+  })
+  .catch(err => {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  });
+
 // import routes
 const authRoutes = require('./routes/auth');
 
